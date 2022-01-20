269dc38b4f74af1ad324ca5f4c9a39b4734cfad4
Andrew Dutson <Dutsandrew@gmail.com>
2022-01-20 14:29:22 -0700

    creating makeRow and defaultGrid functions

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 // variables that manipulate the DOM
-const content = document.querySelector(".content");
-const grid = document.querySelector(".grid");
+const container = document.querySelector("#container");
+
+// creates the 16x16 grid
+function grid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+// function that builds rows using the (rows) input
+function makeRows(rowNumber) {
+    for(r = 0; r < rowNumber; r++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
+    };
+};
