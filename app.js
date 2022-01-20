31af7a5c83f78eeb48c8dca5a2fda94c7cee879b
Andrew Dutson <Dutsandrew@gmail.com>
2022-01-20 14:59:55 -0700

    added row and column builder functions

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 // variables that manipulate the DOM
 const container = document.querySelector("#container");
 
-// creates the 16x16 grid
-function grid() {
+// function to hold the grid. Numbers will be used as arguments in following functions for the amount of columns or rows to build.
+function buildGrid() {
     makeRows(16);
     makeColumns(16);
-}
+};
 
-// function that builds rows using the (rows) input
-function makeRows(rowNumber) {
+// function to build the rows
+function rowBuilder(rowNumber) {
     for(r = 0; r < rowNumber; r++) {
-        let row = document.createElement("div");
+        let row = document.createElement('div');
         container.appendChild(row).className = "gridRow";
+    };
+};
+
+// allows access to rows on the grid, now that the function has created them.
+let rows = document.querySelector('#gridRow');
+
+// function to build columns. Loop for rows is needed in order to build columns: essentially the loop will trace along the row.length and for each iteration it is going to create a column with the column loop. The difference with this function vs the previous rowBuilder() is that rows[c] is being appended. So for each row the loop will place [c] (a column) inside of it. Since the rowNumber and columnNumber are each 16. It will place 16 boxes in each row and until there are 16 columns total.
+function columnBuilder(columnNumber) {
+    for(i = 0; i < rows.length; i++) {
+        for(c = 0; c < columnNumber; c++) {
+            let column = document.createElement('div');
+            rows[c].appendChild(column).className = "gridColumn";
+        };
     };
 };
