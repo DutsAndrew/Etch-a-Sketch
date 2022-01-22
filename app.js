789fdf5bdb3d57ae8d1308505bd4b74d7acdefb5
Andrew Dutson <Dutsandrew@gmail.com>
2022-01-22 13:17:55 -0700

    added onload function and fixed bug in JS

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+// Auto loads a grid that is 16x16 on page open
+window.onload = () => {
+    buildGrid(16);
+}
+
 // variables that manipulate the DOM. "getElement" is used here in place of "querySelector" because querySelector deals with static nodelists, nodelists are like arrays but can't do the same things. In addition, since querySelector is static, the size of the array cannot change; whereas, "getElement" is dynamic, which means the array/list can grow according to needs. Since we are running a loop that is constantly adding to the list we need a dynamic list, which means we use getElement instead of querySelector.
 const container = document.getElementById("container");
 // allows access to rows on the grid, now that the function has created them.
 const rows = document.getElementsByClassName("gridRow");
 // allows access to columns on the grid, now that the function has created them.
-const cells = document.getElementsByClassName("cells");
+const cells = document.getElementsByClassName("cell");
 // allows access to the clear button in HTML
 const clear = document.getElementById("reset");
 // allows access to the newBoard button in HTML
